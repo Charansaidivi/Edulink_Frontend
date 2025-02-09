30dc0874bb998411058f39505a5bcb2e690ad51d
Charansaidivi <charansaideevi@gmail.com>
2025-02-09 17:19:14 +0530

    Add Google authentication to Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import AuthPage from './pages/AuthPage'
 import HomePage from './pages/HomePage'
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateClass from './pages/CreateClass'
+
 const App = () => {
   return (
-    <div>
-       <Routes>
+    <GoogleOAuthProvider clientId="625626669431-c2p74fbeko7t33f236vooiu3sn3d9nvq.apps.googleusercontent.com">
+      <div>
+        <Routes>
           <Route path='/' element={<AuthPage/>}/>
           <Route path='/home' element={<HomePage/>}/>
           <Route path='/create-class' element={<CreateClass/>}/>
-       </Routes>
-    </div>
+        </Routes>
+      </div>
+    </GoogleOAuthProvider>
   )
 }
+
 export default App

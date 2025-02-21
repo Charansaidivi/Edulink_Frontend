f9af443eb8bb7e7a24575ace7d28eff8fbf77934
CharanSmart2003 <pappucharannarayanaswamy@gmail.com>
2025-02-21 14:35:24 +0530

    Add selected files and modifications

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+function LandingPage() {
+    return (
+        <div>
+            <img src="path/to/Group.png" alt="Group Illustration" />
+            <h1>Edulinker</h1>
+            <p>Description: Student Learning Platform, Share your knowledge, Learn from Friends</p>
+        </div>
+    );
+} 
